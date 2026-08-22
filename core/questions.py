c89b35aff0ecd37742b27881f5b90fc417db22bb
Timothy Nakev <timothy.nakev@gmail.com>
2026-08-22 16:40:27 -0400

    clean up old code and update json loading method

--- core/questions.py
+++ core/questions.py
@@ -1,31 +1,23 @@
 import random
-import json
-import sys
 
-with open("json/vocab.json", "r") as file:
-    master_list = json.load(file)
-
-length = len(master_list)
-word_choice = 0
-
-def ask_question(question_type):
-    if question_type == "meaning":
-        word_choice = random.randrange(length)
-        print(f'Which selection best represents the meaning of the word {master_list[word_choice]["word"]}?\n')
-        correct_answer = random.randint(1, 4)
-        for i in range(4):
-            if i + 1 != correct_answer:
-                temp_randnum = random.randrange(length)
-                while temp_randnum == word_choice:
-                    temp_randnum = random.randrange(length)
-                print(f'\t{i + 1}. {master_list[temp_randnum]["meaning"]}')
-            else:
-                print(f'\t{i + 1}. {master_list[word_choice]["meaning"]}')
-        choice = int(input(""))
-        if choice == correct_answer:
-            print("Correct")
-        else:
-            print("Incorrect")
+#def ask_question(question_type):
+#    if question_type == "meaning":
+#        word_choice = random.randrange(length)
+#        print(f'Which selection best represents the meaning of the word {master_list[word_choice]["word"]}?\n')
+#        correct_answer = random.randint(1, 4)
+#        for i in range(4):
+#            if i + 1 != correct_answer:
+#                temp_randnum = random.randrange(length)
+#                while temp_randnum == word_choice:
+#                    temp_randnum = random.randrange(length)
+#                print(f'\t{i + 1}. {master_list[temp_randnum]["meaning"]}')
+#            else:
+#                print(f'\t{i + 1}. {master_list[word_choice]["meaning"]}')
+#        choice = int(input(""))
+#        if choice == correct_answer:
+#            print("Correct")
+#        else:
+#            print("Incorrect")
 
 class Quiz:
     def __init__(self, master_list):
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import json
 import sys
-
-from core.questions import Quiz, ask_question
+import os
+from core.questions import Quiz
 from ui.main_window import MainWindow
 from PyQt6.QtWidgets import QApplication
 
-with open("json/vocab.json", "r") as file:
+def fetch_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+json_path = fetch_path(os.path.join("json", "vocab.json"))
+
+with open(json_path, "r", encoding="utf-8") as file:
     master_list = json.load(file)
 
 quiz = Quiz(master_list)
